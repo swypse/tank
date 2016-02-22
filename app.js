50de2894c504f9e152eba4fb395f1256721550a0
skink-in-trees-shade <whathaveyoudone@hushmail.com>
2016-02-22 16:52:21 +0200

    Moar drawing

--- app.js
+++ app.js
@@ -6,14 +6,20 @@ import {Context} from './core/context';
 
 var ui = new Ui("ui");
 
-var a = new Actor("actor", 10, 20);
+var a = new Actor("actor", 10, 20, 10, 10, 'line', '#ff0000', 'fill');
 ui.addActor(a);
 
-var a2 = new Actor("actor2", 70, 10);
+var a2 = new Actor("actor2", 70, 10, 20, 20, 'rectangle', '#00ff00', 'fill');
 ui.addActor(a2);
 
-var a3 = new Actor("actor3", 100, 150);
+var a3 = new Actor("actor3", 100, 150, 30, 30, 'rectangle', '#0000ff', 'stroke');
 ui.addActor(a3);
+
+var a4 = new Actor("actor4", 160, 50, 20, 20, 'ellipse', '#ffff00', 'stroke');
+ui.addActor(a4);
+
+var a5 = new Actor("actor5", 70, 70, 20, 20, 'circle', '#00ffff', 'fill');
+ui.addActor(a5);
 
 var s = new Scene("scene");
 s.addLayer(ui);
--- core/actor.js
+++ core/actor.js
@@ -1,11 +1,36 @@
 export class Actor {
-	constructor(name, x, y) {
+	constructor(name, x, y, width, height, primitiveType, color, drawingType) {
 		this.name = name;
 		this.x = x;
 		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.primitiveType = primitiveType;
+		this.color = color;
+		this.drawingType = drawingType;
 	}
 	
 	draw(ctx) {
-		ctx.rectangle(this.x, this.y, 10, 10, '#ff0000');
+		switch (this.primitiveType)
+		{
+			case 'rectangle':
+				ctx.rectangle(this.x, this.y, this.width, this.height, this.color, this.drawingType);
+				return;
+
+			case 'line':
+				ctx.line(this.x, this.y, this.x + this.width, this.y + this.height, this.color);
+				return;
+
+			case 'ellipse':
+				ctx.ellipse(this.x + this.width / 2, this.y + this.height / 2, this.width, this.height, this.color, this.drawingType);
+				return;
+
+			case 'circle':
+				ctx.circle(this.x + this.width / 2, this.y + this.height / 2, this.width, this.color, this.drawingType);
+				return;
+
+			default:
+				throw `Unknown primitive type '${this.primitiveType}'`;
+		}
 	}
 }
