f41b3e3dff1511ee050814b499e8827b7f44b5bb
Swypse <swypse@gmail.com>
2016-02-24 12:55:27 +0200

    game.js не помню, что делал

--- src/game.js
+++ src/game.js
@@ -1,27 +1,5 @@
 'use strict';
 
-/*
-import {Scene} from './scene'
-import {Renderer} from './renderer';
-var gameInterval = null,
-	gameSpeed = 10;
-
-export class Game {
-	constructor(name, renderer) {
-		this.name = name;
-		this.renderer = renderer;
-	}
-	
-	setScene(scene) {
-		this.scene = scene;
-	}
-	
-	start() {
-		gameInterval = setInterval(() => { 
-			this.renderer.render(this.scene);
-		 }, gameSpeed);
-	}
-*/
 import {Scene} from './scene'
 import {Loop} from './loop'
 
